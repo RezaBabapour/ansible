af10016e45f4fcd1d41ee4ec2b309b95803ba80c
reza babapour <reza.babapour91@gmail.com>
2023-02-04 13:19:27 +0100

    Add run_service role

--- scripts/run_service.sh
+++ scripts/run_service.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-#ansible-playbook -i inventory/services.yml -i inventory/servers.yml run_service.yml -e service=$1 --limit=$1
 ansible-playbook -i inventory/servers.yml -i inventory/services.yml run_service.yml -e service=$1 --limit=$1
